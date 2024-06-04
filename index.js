a7ccc1d5596508531860204638e6d41da278e67e
ovi246 <sbswag9@gmail.com>
2024-06-05 03:23:12 +0600

    changed await sendmail

--- index.js
+++ index.js
@@ -248,12 +248,16 @@ app.post("/submit-review", async (req, res) => {
       //   }
       // });
 
-      transporter.sendMail(userMailOptions, (error, info) => {
-        if (error) {
-          console.error("Error sending email to user:", error);
-        } else {
-          console.log("Email sent to user:", info);
-        }
+      await new Promise((resolve, reject) => {
+        transporter.sendMail(userMailOptions, (error, info) => {
+          if (error) {
+            console.error("Error sending email to user:", error);
+            reject(error);
+          } else {
+            console.log("Email sent to user:", info);
+            resolve(info);
+          }
+        });
       });
 
       res
