b5fdee17743ddea5ada468efe20ec56ead762a7b
ovi246 <sbswag9@gmail.com>
2024-06-05 02:15:47 +0600

    changed email sending code

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const serviceAccount = {
   type: "service_account",
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
@@ -226,25 +226,21 @@ app.post("/submit-review", async (req, res) => {
       };
 
       // Send email to the user
-      await new Promise((resolve, reject) => {
-        transporter.sendMail(userMailOptions, (error, info) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(info);
-          }
-        });
+      transporter.sendMail(userMailOptions, (error, info) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(info);
+        }
       });
 
       // Send email to the admin
-      await new Promise((resolve, reject) => {
-        transporter.sendMail(adminMailOptions, (error, info) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(info);
-          }
-        });
+      transporter.sendMail(adminMailOptions, (error, info) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(info);
+        }
       });
 
       res
